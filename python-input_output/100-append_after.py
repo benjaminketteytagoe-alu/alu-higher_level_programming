824bf70736745957e12f94e3d3f872259f66f525
benjaminketteytagoe-alu <b.kettey-ta@alustudent.com>
2025-07-14 05:30:47 -0700

    100-append_after.py

---
+++ python-input_output/100-append_after.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Module that contains append_after function
+"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """
+    Inserts a line of text to a file, after each line containing a specific string
+    
+    Args:
+        filename (str): The name of the file to modify
+        search_string (str): The string to search for in each line
+        new_string (str): The string to insert after matching lines
+    """
+    with open(filename, 'r') as f:
+        lines = f.readlines()
+    
+    with open(filename, 'w') as f:
+        for line in lines:
+            f.write(line)
+            if search_string in line:
+                f.write(new_string)
