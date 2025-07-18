05677e93022663b9afa0ae03d19e29539bd0d40e
benjaminketteytagoe-alu <b.kettey-ta@alustudent.com>
2025-07-17 22:11:47 -0700

    modified

--- SQL_introduction/100-move_to_utf8.sql
+++ SQL_introduction/100-move_to_utf8.sql
@@ -8,6 +8,3 @@ USE hbtn_0c_0;
 
 -- Convert table to UTF8
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
--- Convert specific field to UTF8 (this is redundant after table conversion, but included for completeness)
-ALTER TABLE first_table MODIFY name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
