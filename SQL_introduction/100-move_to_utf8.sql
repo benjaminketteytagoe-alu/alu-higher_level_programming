e1160523856109441ede7e868c97a2bf3b5eca58
benjaminketteytagoe-alu <b.kettey-ta@alustudent.com>
2025-07-17 22:08:04 -0700

    100-move_to_utf8.sql

---
+++ SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,10 @@
+-- Script to convert hbtn_0c_0 database to UTF8 (utf8mb4, collate utf8mb4_unicode_ci)
+
+-- Convert database to UTF8
+ALTER DATABASE hbtn_0c_0 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
+
+-- Convert table to UTF8
+ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- Convert specific field to UTF8 (this is redundant after table conversion, but included for completeness)
+ALTER TABLE first_table MODIFY name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
