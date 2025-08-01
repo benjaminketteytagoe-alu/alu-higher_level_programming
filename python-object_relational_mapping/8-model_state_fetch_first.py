7c20cd616ffc2b06d9d1379f9be318b4e56f4af7
benjaminketteytagoe-alu <b.kettey-ta@alustudent.com>
2025-07-31 23:48:39 -0700

    modified

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -13,25 +13,25 @@ if __name__ == "__main__":
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
-    
+
     # Create engine to connect to MySQL server
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
         username, password, database), pool_pre_ping=True)
-    
+
     # Create a configured "Session" class
     Session = sessionmaker(bind=engine)
-    
+
     # Create a Session
     session = Session()
-    
+
     # Query for the first state ordered by id
     first_state = session.query(State).order_by(State.id).first()
-    
+
     # Display result
     if first_state is None:
         print("Nothing")
     else:
         print("{}: {}".format(first_state.id, first_state.name))
-    
+
     # Close the session
     session.close()
