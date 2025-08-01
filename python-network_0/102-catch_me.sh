98a9ea12a143d6a82ed95db4cdd81451d59442f8
benjaminketteytagoe-alu <b.kettey-ta@alustudent.com>
2025-08-01 13:04:56 -0700

    102-catch_me.sh

--- python-network_0/102-catch_me.sh
+++ python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script that makes a request to catch_me endpoint that returns "You got me!"
-curl -s -X PUT -H "Origin: HolbertonSchool" -d "user_id=98" 0.0.0.0:5000/catch_me
+curl -s -L -X PUT -H "Origin: HolbertonSchool" -d "user_id=98" 0.0.0.0:5000/catch_me
