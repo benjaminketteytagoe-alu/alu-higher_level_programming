c27948b458e0051c3292db739cf9cdf604ce7e91
benjaminketteytagoe-alu <b.kettey-ta@alustudent.com>
2025-07-31 23:31:44 -0700

    3-my_safe_filter_states.py

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -12,15 +12,16 @@ import sys
 def main():
     """Main function to connect to MySQL and display filtered states"""
     if len(sys.argv) != 5:
-        print("Usage: {} <mysql_username> <mysql_password> <database_name> <state_name>".format(sys.argv[0]))
+        print("Usage: {} <mysql_username> <mysql_password> "
+              "<database_name> <state_name>".format(sys.argv[0]))
         sys.exit(1)
-    
+
     # Get command line arguments
     mysql_username = sys.argv[1]
     mysql_password = sys.argv[2]
     database_name = sys.argv[3]
     state_name = sys.argv[4]
-    
+
     try:
         # Connect to MySQL server
         db = MySQLdb.connect(
@@ -30,26 +31,26 @@ def main():
             passwd=mysql_password,
             db=database_name
         )
-        
+
         # Create cursor object
         cursor = db.cursor()
-        
+
         # Use parameterized query to prevent SQL injection
         # The %s placeholder will be safely escaped by MySQLdb
         query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
         cursor.execute(query, (state_name,))
-        
+
         # Fetch all results
         results = cursor.fetchall()
-        
+
         # Display results
         for row in results:
             print(row)
-        
+
         # Close cursor and database connection
         cursor.close()
         db.close()
-        
+
     except MySQLdb.Error as e:
         print("MySQL Error: {}".format(e))
         sys.exit(1)
