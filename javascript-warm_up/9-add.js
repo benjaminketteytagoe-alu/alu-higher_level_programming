e8ac41677a0e3b6516bd9aa36568a31ce5cf4c05
benjaminketteytagoe-alu <b.kettey-ta@alustudent.com>
2025-09-29 21:12:34 -0700

    modified

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -1,9 +1,6 @@
 #!/usr/bin/node
 function add (a, b) {
-	  return (a + b);
+  return a + b;
 }
-const argv = process.argv.slice(2);
-const y = parseInt(argv[0]);
-const z = parseInt(argv[1]);
 
-console.log(add(y, z));
+console.log(add(parseInt(process.argv[2]), parseInt(process.argv[3])));
