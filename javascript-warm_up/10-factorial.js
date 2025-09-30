aa78c94cc48a51237d8efd8f7ae0633389eee6cd
benjaminketteytagoe-alu <b.kettey-ta@alustudent.com>
2025-09-29 21:15:21 -0700

    modified

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,12 +1,9 @@
 #!/usr/bin/node
-
 function factorial (n) {
-	  if (isNaN(n)) return 1; // Factorial of NaN is 1
-	  if (n <= 1) return 1; // Base case: factorial of 0 or 1 is 1
-	  return n * factorial(n - 1);
+  if (isNaN(n) || n <= 1) {
+    return 1;
+  }
+  return n * factorial(n - 1);
 }
 
-// Get the first argument, cast it to integer
-const arg = parseInt(process.argv[2]);
-
-console.log(factorial(arg));
+console.log(factorial(parseInt(process.argv[2])));
