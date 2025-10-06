357b2e3acfba4e0e4f39c723780df1751b92e8a0
benjaminketteytagoe-alu <b.kettey-ta@alustudent.com>
2025-10-06 07:17:14 -0700

    modified

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -1,22 +1,36 @@
 #!/usr/bin/node
 
 const request = require('request');
-const url = process.argv[2];
-request.get(url, function (err, response, body) {
-	  if (err) {
-		      console.log(err);
-		    } else {
-			        const todos = JSON.parse(body);
-			        const completed = {};
-			        for (const x of todos) {
-					      if (x.completed === true) {
-						              if (x.userId in completed) {
-								                completed[x.userId]++;
-								              } else {
-										                completed[x.userId] = 1;
-										              }
-						            }
-					    }
-			        console.log(completed);
-			      }
+
+// Get the API URL from command line arguments
+const apiUrl = process.argv[2];
+
+// Make a GET request to the API
+request(apiUrl, (error, response, body) => {
+  if (error) {
+    // Print the error if one occurred
+    console.log(error);
+  } else {
+    // Parse the JSON response
+    const todos = JSON.parse(body);
+
+    // Object to store completed tasks count per user
+    const completedTasks = {};
+
+    // Iterate through all todos
+    todos.forEach((todo) => {
+      // Only count completed tasks
+      if (todo.completed) {
+        // Initialize counter for user if not exists
+        if (!completedTasks[todo.userId]) {
+          completedTasks[todo.userId] = 0;
+        }
+        // Increment the count for this user
+        completedTasks[todo.userId]++;
+      }
+    });
+
+    // Print the completed tasks object
+    console.log(completedTasks);
+  }
 });
