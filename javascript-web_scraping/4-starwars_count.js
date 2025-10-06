3dfbc1aa8a87c5c32c9d30c9f5140d128ab3305d
benjaminketteytagoe-alu <b.kettey-ta@alustudent.com>
2025-10-06 06:55:00 -0700

    modified

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -1,12 +1,36 @@
 #!/usr/bin/node
+
 const request = require('request');
-request(process.argv[2], function (error, response, body) {
-	  if (!error) {
-		      const results = JSON.parse(body).results;
-		      console.log(results.reduce((count, movie) => {
-			            return movie.characters.find((character) => character.endsWith('/18/'))
-			              ? count + 1
-			              : count;
-			          }, 0));
-		    }
+
+// Get the API URL from command line arguments
+const apiUrl = process.argv[2];
+
+// Wedge Antilles character ID
+const characterId = '18';
+
+// Make a GET request to the Star Wars API
+request(apiUrl, (error, response, body) => {
+  if (error) {
+    // Print the error if one occurred
+    console.log(error);
+  } else {
+    // Parse the JSON response
+    const data = JSON.parse(body);
+    
+    // Count movies where Wedge Antilles appears
+    let count = 0;
+    
+    // Iterate through all films
+    data.results.forEach((film) => {
+      // Check if character ID 18 is in the characters array
+      film.characters.forEach((characterUrl) => {
+        if (characterUrl.includes(`/people/${characterId}/`)) {
+          count++;
+        }
+      });
+    });
+    
+    // Print the count
+    console.log(count);
+  }
 });
