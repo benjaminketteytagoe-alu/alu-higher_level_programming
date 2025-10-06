ab35e33cdc8dfbc2ffc90ab5f8a08e4812a871c9
benjaminketteytagoe-alu <b.kettey-ta@alustudent.com>
2025-10-06 06:47:28 -0700

    modified

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -1,9 +1,17 @@
 #!/usr/bin/node
 
 const request = require('request');
+
+// Get the URL from command line arguments
 const url = process.argv[2];
-request
-  .get(url)
-  .on('response', function (response) {
-	      console.log('code: ' + response.statusCode);
-	    });
+
+// Make a GET request to the URL
+request(url, (error, response) => {
+  if (error) {
+    // Print the error if one occurred
+    console.log(error);
+  } else {
+    // Print the status code
+    console.log(`code: ${response.statusCode}`);
+  }
+});
