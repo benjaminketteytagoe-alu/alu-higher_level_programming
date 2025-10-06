3e86caefabf58b3c05a2e3681ae72977cd17b732
benjaminketteytagoe-alu <b.kettey-ta@alustudent.com>
2025-10-06 05:28:23 -0700

    modified

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,11 +1,17 @@
 #!/usr/bin/node
 
 const fs = require('fs');
-const FileName = process.argv[2];
-fs.readFile(FileName, 'utf8', function (err, line) {
-	  if (err) {
-		      console.log(err);
-		    } else {
-			        console.log(line);
-			      }
+
+// Get the file path from command line arguments
+const filePath = process.argv[2];
+
+// Read the file with utf-8 encoding
+fs.readFile(filePath, 'utf-8', (err, data) => {
+  if (err) {
+    // Print the error object if an error occurred
+    console.log(err);
+  } else {
+    // Print the file content
+    console.log(data);
+  }
 });
