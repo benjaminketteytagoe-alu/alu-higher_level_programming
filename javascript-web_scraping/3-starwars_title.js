c550569c47be5ce8cd2971195a2315e950d73000
benjaminketteytagoe-alu <b.kettey-ta@alustudent.com>
2025-10-06 06:49:46 -0700

    modified

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -1,12 +1,21 @@
 #!/usr/bin/node
 
 const request = require('request');
-const MovieId = process.argv[2];
-const url = 'https://swapi-api.hbtn.io/api/films/' + MovieId;
-request.get(url, function (err, response, body) {
-	  if (err) {
-		      console.log(err);
-		    } else {
-			        console.log(JSON.parse(body).title);
-			      }
+
+// Get the movie ID from command line arguments
+const movieId = process.argv[2];
+
+// Construct the API URL
+const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+
+// Make a GET request to the Star Wars API
+request(url, (error, response, body) => {
+  if (error) {
+    // Print the error if one occurred
+    console.log(error);
+  } else {
+    // Parse the JSON response and print the movie title
+    const movieData = JSON.parse(body);
+    console.log(movieData.title);
+  }
 });
