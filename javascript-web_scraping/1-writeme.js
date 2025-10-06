a7f5d60b152ec94b37ca451c99408ecd403f32b8
benjaminketteytagoe-alu <b.kettey-ta@alustudent.com>
2025-10-06 06:45:35 -0700

    modified

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -1,11 +1,15 @@
 #!/usr/bin/node
 
 const fs = require('fs');
-const FileName = process.argv[2];
-const FileContent = process.argv[3];
 
-fs.writeFile(FileName, FileContent, function (err) {
-	  if (err) {
-		      console.log(err);
-		    }
+// Get the file path and content from command line arguments
+const filePath = process.argv[2];
+const content = process.argv[3];
+
+// Write the content to the file with utf-8 encoding
+fs.writeFile(filePath, content, 'utf-8', (err) => {
+  if (err) {
+    // Print the error object if an error occurred
+    console.log(err);
+  }
 });
